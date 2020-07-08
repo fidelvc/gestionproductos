aeea73142fa95e30aab355ee0646dff2b1b38b78
fidelvc <fidel@qtech.es>
2020-07-08 17:59:07 +0200

    add new component alta-producto, new model producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AltaProductoComponent } from './components/alta-producto/alta-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltaProductoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
